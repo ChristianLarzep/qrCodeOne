3a562a367df5f4ca8a631a1871d789be828b8891
Christian Larzep <christian.dm2@hotmail.com>
2018-10-12 13:48:43 -0500

    api

--- src/app/services/invitation.services.ts
+++ src/app/services/invitation.services.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/Observable';
 import { Invitation } from '../models/invitation';
 
 @Injectable()
@@ -10,18 +9,18 @@ export class InvitationService{
   public deezerAPI : string;
 
   constructor( public http:Http){
-    this.deezerAPI = "/invitationsAPI/"; //proxy en ionic.config.json, ahi esta la url
+    this.myAPI = "/invitationsAPI/"; //proxy en ionic.config.json, ahi esta la url
   }
 
   public sendInvitation(invitation: Invitation){
     let params = JSON.stringify(invitation);
     let headers = new Headers({'Content-Type':  'application/json'});
-    return this.http.post(this.deezerAPI+'invitation', params, {headers: headers})
+    return this.http.post(this.myAPI+'invitation', params, {headers: headers})
                           .map(res => res);
   }
 
 getInvitations(){
-  return this.http.get(this.deezerAPI+'invitation')
+  return this.http.get(this.myAPI+'invitation')
                   .map(res => res.json())
 }
 
